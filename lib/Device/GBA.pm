9892b8718a1556514de0d50f6d2d6b13ad02a7f1
Ahmad Fatoum <ahmad@a3f.at>
2018-06-06 23:44:49 +0200

    Use Device::Chip::Adapter::BusPirate instead of Device::BusPirate

--- lib/Device/GBA.pm
+++ lib/Device/GBA.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use integer;
 use Time::HiRes;
-use Device::BusPirate v0.15;
+use Device::Chip::Adapter::BusPirate v0.15;
 use File::stat;
 use Term::ProgressBar;
 
@@ -29,13 +29,14 @@ Device::GBA - Perl Interface to the Gameboy Advance
 
 =head1 DESCRIPTION
 
-The Nintendo Gameboy Advance can either boot from cartridge or over link cable. The latter is caled multiboot mode and is basically SPI and a homebrew encoding scheme. Unfortunately, the Bus Pirate doesn't have a 100k SPI mode, so we are using 125k instead. If you encounter problems with booting, use the next lower speed (30k) as bitrate.
-This utility allows uploading multiboot GBA images with the L<BusPirate|Device::BusPirate>. Don't forget to pass C<-specs=gba_mb.specs> to devkitARM GCC if you want to link a multiboot image. The package's C<share/> subdirectory contains an L<example Makefile|https://github.com/athreef/Device-GBA/blob/master/share/testimg/Makefile> for cross-compilation. The wiring is as follows:
+The Nintendo Gameboy Advance can either boot from cartridge or over link cable. The latter is caled multiboot mode and is basically SPI and a homebrew encoding scheme. Unfortunately, the Bus Pirate doesn't have a 100k SPI mode, so we are using 125000 instead. If you encounter problems with booting, use the next lower speed (30000) as bitrate.
+This utility allows uploading multiboot GBA images via L<Device::Chip::Adapter>s. Don't forget to pass C<-specs=gba_mb.specs> to devkitARM GCC if you want to link a multiboot image. The package's C<share/> subdirectory contains an L<example Makefile|https://github.com/athreef/Device-GBA/blob/master/share/testimg/Makefile> for cross-compilation. The wiring is as follows:
 
     GBA     Bus Pirate
     SO  --> MISO
     SI  <-- MOSI
     CLK <-- CLK
+    GND --- GND
 
 Note: This is still work in progress!
 
@@ -50,9 +51,9 @@ if an attempt to open the device has failed. Accepts following parameters:
 
 =over 4
 
-=item B<buspirate>
+=item B<adapter>
 
-COM port or handle of the BusPirate connected to the Gameboy Advance.
+An instance of L<Device::Chip::Adapter> capable of SPI communication.
 
 =item B<verbose>
 
@@ -66,16 +67,11 @@ sub new {
     my $class = shift;
     my $self = {
         verbose => 0,
-        bitrate   => '125k',
+        bitrate   => '125000',
         @_
     };
 
     $self->{log} = $self->{verbose} ? sub { printf @_ } : sub { };
-
-    if (ref $self->{buspirate} ne 'Device::BusPirate') {
-        $self->{buspirate} = Device::BusPirate->new(serial => $self->{buspirate}, %$self)
-           or return;
-    }
 
     enter_spi($self);
 
@@ -88,8 +84,8 @@ sub enter_spi
    my $self = shift;
    return if defined $self->{spi};
 
-   $self->{spi} = $self->{buspirate}->enter_mode( "SPI" )->get;
-   $self->{spi}->configure(mode => 3, speed => $self->{bitrate})->get;
+   $self->{spi} = $self->{adapter}->make_protocol("SPI")->get;
+   $self->{spi}->configure(mode => 3, max_bitrate => $self->{bitrate})->get;
 }
 
 =item upload
@@ -124,26 +120,26 @@ sub upload {
 
     $self->spi_handshake(0x00006202, 0x72026202, "Looking for GBA");
 
-    $self->spi_writeread(0x00006202, "Found GBA");
-    $self->spi_writeread(0x00006102, "Recognition OK");
+    $self->spi_readwrite(0x00006202, "Found GBA");
+    $self->spi_readwrite(0x00006102, "Recognition OK");
 
     my $fcnt;
     for($fcnt = 0; $fcnt < 192; $fcnt += 2) {
-        $self->spi_writeread(unpack 'S<', <$fh>);
+        $self->spi_readwrite(unpack 'S<', <$fh>);
     }
 
-    $self->spi_writeread(0x00006200, "Transfer of header data complete");
-    $self->spi_writeread(0x00006202, "Exchange master/slave info again");
+    $self->spi_readwrite(0x00006200, "Transfer of header data complete");
+    $self->spi_readwrite(0x00006202, "Exchange master/slave info again");
 
-    $self->spi_writeread(0x000063d1, "Send palette data");
+    $self->spi_readwrite(0x000063d1, "Send palette data");
 
-    my $r = $self->spi_writeread(0x000063d1, "Send palette data, receive 0x73hh****");
+    my $r = $self->spi_readwrite(0x000063d1, "Send palette data, receive 0x73hh****");
 
     my $m = (($r & 0x00ff0000) >>  8) + 0xffff00d1;
     my $h = (($r & 0x00ff0000) >> 16) + 0xf;
 
-    $r = $self->spi_writeread(((($r >> 16) + 0xf) & 0xff) | 0x00006400, "Send handshake data");
-    $r = $self->spi_writeread(($fsize - 0x190) / 4, "Send length info, receive seed 0x**cc****");
+    $r = $self->spi_readwrite(((($r >> 16) + 0xf) & 0xff) | 0x00006400, "Send handshake data");
+    $r = $self->spi_readwrite(($fsize - 0x190) / 4, "Send length info, receive seed 0x**cc****");
 
     my $f = ((($r & 0x00ff0000) >> 8) + $h) | 0xffff0000;
     my $c = 0x0000c387;
@@ -164,7 +160,7 @@ sub upload {
         $c = crc($w, $c);
         $m = ((0x6f646573 * $m) & 0xFFFFFFFF) + 1;
         my $data = $w ^ ((~(0x02000000 + $fcnt)) + 1) ^ $m ^ 0x43202f2f;
-        $self->spi_writeread($data);
+        $self->spi_readwrite($data);
 
         $progress->update($fcnt);
     }
@@ -173,26 +169,26 @@ sub upload {
 
     $self->spi_handshake(0x00000065, 0x00750065, "\nWait for GBA to respond with CRC");
 
-    $self->spi_writeread(0x00000066, "GBA ready with CRC");
-    $self->spi_writeread($c,         "Let's exchange CRC!");
+    $self->spi_readwrite(0x00000066, "GBA ready with CRC");
+    $self->spi_readwrite($c,         "Let's exchange CRC!");
 
     $self->log("CRC ...hope they match!\n");
     $self->log("MultiBoot done\n");
 }
 
-=item spi_writeread
+=item spi_readwrite
 
-    $miso = $gba->spi_writeread($mosi)
+    $miso = $gba->spi_readwrite($mosi)
 
 reads and writes 32 bit from the SPI bus.
 
 =cut
 
-sub spi_writeread {
+sub spi_readwrite {
     my $self = shift;
     my ($w, $msg) = @_;
     $self->enter_spi;
-    my $r = unpack 'L>', $self->{spi}->writeread(pack 'L>', shift)->get;
+    my $r = unpack 'L>', $self->{spi}->readwrite(pack 'L>', shift)->get;
     $self->log("0x%08x 0x%08x  ; %s\n", $r , $w, $msg) if defined $msg;
     return $r;
 }
@@ -202,7 +198,7 @@ sub spi_handshake {
     my ($w, $expected, $msg) = @_;
     $self->log("%s 0x%08x\n", $msg, $expected) if defined $msg;
 
-    while ($self->spi_writeread($w) != $expected) {
+    while ($self->spi_readwrite($w) != $expected) {
         sleep 0.01;
     }
 }
